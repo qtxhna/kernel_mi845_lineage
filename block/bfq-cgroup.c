b3adac3da66609b94036a473621cb9078f2e9a49
awkoo <184658409+awkoo@users.noreply.github.com>
2026-03-07 17:00:39 +0800

    Revert "block, bfq: add Early Queue Merge (EQM) to BFQ-v7r11 for 4.5.0"
    This reverts commit 24995f0db5a4b2f5e0145dd6de496c4d09c46de5.

--- block/bfq-cgroup.c
+++ block/bfq-cgroup.c
@@ -440,7 +440,6 @@ static void bfq_pd_init(struct blkg_policy_data *pd)
 				   */
 	bfqg->bfqd = bfqd;
 	bfqg->active_entities = 0;
-	bfqg->rq_pos_tree = RB_ROOT;
 }
 
 static void bfq_pd_free(struct blkg_policy_data *pd)
@@ -534,9 +533,6 @@ static struct bfq_group *bfq_find_alloc_group(struct bfq_data *bfqd,
 	return bfqg;
 }
 
-static void bfq_pos_tree_add_move(struct bfq_data *bfqd,
-				  struct bfq_queue *bfqq);
-
 /**
  * bfq_bfqq_move - migrate @bfqq to @bfqg.
  * @bfqd: queue descriptor.
@@ -584,7 +580,6 @@ static void bfq_bfqq_move(struct bfq_data *bfqd, struct bfq_queue *bfqq,
 	bfqg_get(bfqg);
 
 	if (busy) {
-		bfq_pos_tree_add_move(bfqd, bfqq);
 		if (resume)
 			bfq_activate_bfqq(bfqd, bfqq);
 	}
